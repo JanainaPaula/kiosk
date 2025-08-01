6ec66e535b2593ac31dc78506f6093c163c915c6
Janaina Paula <janainadepaula93@gmail.com>
2025-08-01 20:37:18 -0300

    add entidades de domínio Menu e Item

---
+++ kiosk-menu-ms/src/domain/item.ts
@@ -0,0 +1,9 @@
+type Item = {
+    id: number;
+    name: string;
+    description: string;
+    image: boolean;
+    price: number;
+  };
+  
+  export default Item;
---
+++ kiosk-menu-ms/src/domain/menu.ts
@@ -0,0 +1,7 @@
+import Item from "./item"
+
+type Menu = {
+    itens: Item[]
+};
+
+export default Menu;
